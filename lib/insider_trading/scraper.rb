5fcf760a85e6e13534acd11f95993f9d23d378a8
Lucas <lqf2b8@gmail.com>
2017-09-21 12:14:06 -0500

    figured out the annoying space issue and fixed spelling error

--- lib/insider_trading/scraper.rb
+++ lib/insider_trading/scraper.rb
@@ -3,7 +3,7 @@ class InsiderTrading::Scraper
 		basic_doc = Nokogiri::HTML(open(stock.basic_url))
     	insider_doc = Nokogiri::HTML(open(stock.insider_url))
     	stock.name = basic_doc.search("h1").text.strip.gsub(" Quote & Summary Data", "")
-    	stock.price = basic_doc.search("div.qwidget-dollar").text.gsub("*", "").strip
+    	stock.price = basic_doc.search("div.qwidget-dollar").text.strip.gsub("*" && "$", "").to_f.round(2)
     	stock.insider3 = insider_doc.search("td.center")[14].text 
     	stock.insider12 = insider_doc.search("td.center")[15].text
 
